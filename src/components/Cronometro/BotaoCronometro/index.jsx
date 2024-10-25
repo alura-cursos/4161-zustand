9cdb49667c40b4e488852978941fa72b4d8f1a99
monicahillman <mmhillman1@ucs.br>
2024-10-25 08:49:51 -0300

    5.1

--- src/components/Cronometro/BotaoCronometro/index.jsx
+++ src/components/Cronometro/BotaoCronometro/index.jsx
@@ -8,13 +8,22 @@ import { useCronometroStore } from "../../../store";
 export default function BotaoCronometro() {
   const intervaloId = useCronometroStore((estado) => estado.intervaloId);
   const iniciarCronometro = useCronometroStore((estado) => estado.iniciarCronometro);
+  const pausarCronometro = useCronometroStore((estado) => estado.pausarCronometro);
+
+  function iniciarOuPausar() {
+    if (!intervaloId) {
+      iniciarCronometro();
+    } else {
+      pausarCronometro();
+    }
+  }
 
   const textoIniciarOuPausar = intervaloId ? "Pausar" : "Começar";
   const iconeIniciarOuPausar = intervaloId ? pauseImg : play_arrowImg;
 
   return (
     <div className={styles["cronometer__primary-button-wrapper"]}>
-      <button className={styles["cronometer__primary-button"]} onClick={iniciarCronometro}>
+      <button className={styles["cronometer__primary-button"]} onClick={iniciarOuPausar}>
         <img
           className={styles["cronometer__primary-button-icon"]}
           src={iconeIniciarOuPausar}
